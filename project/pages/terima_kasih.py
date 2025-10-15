e0046666a09efe4512bcdcc102fab3e509147151
Tenmaaa <email_githubmu@example.com>
2025-10-15 09:40:32 +0800

    tambah terima kasih

--- project/pages/terima_kasih.py
+++ project/pages/terima_kasih.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
 st.title("Terima Kasih telah mengisi, jawaban anda sangat penting untuk penelitian ini :)")
+for key in st.session_state.keys():
+            del st.session_state[key]
